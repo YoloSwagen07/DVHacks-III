d7089133e16aef22184c3cf61198eac53d52f099
YoloSwagen07 <45905827+YoloSwagen07@users.noreply.github.com>
2021-03-20 10:54:36 -0700

    Update Light_Sensor_Arduino_Hackathon_2021.ino

--- Light_Sensor_Arduino_Hackathon_2021.ino
+++ Light_Sensor_Arduino_Hackathon_2021.ino
@@ -1,16 +1,19 @@
 
 const int lightsensor = A0;
 const int blueLED = 9;
+const int buzzer = 8;
 int hella;
 boolean bet = false;
 void setup() {
   Serial.begin(9600);
   pinMode(blueLED, OUTPUT);
   pinMode(lightsensor, INPUT);
+  pinMode(buzzer, OUTPUT);
 }
 
 void loop() {
      digitalWrite(blueLED, LOW);
+     digitialWrite(buzzer, LOW);
      hella = analogRead(lightsensor);
      if(bet == false){
      }
@@ -19,13 +22,16 @@ void loop() {
         Serial.println("Fire");
         Serial.println(hella);
         digitalWrite(blueLED, HIGH);
+        digitialWrite(buzzer, HIGH);
         delay(200);
         digitalWrite(blueLED, LOW);
+        digitialWrite(buzzer, LOW);
         delay(200);
         }
         
      else { 
       digitalWrite(blueLED, LOW);
+      digitialWrite(buzzer, LOW);
       bet = false;
      }
    }
